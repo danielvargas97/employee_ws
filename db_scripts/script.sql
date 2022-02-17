7f9a85c2bb60e699d085db4340b89552cb24d4da
Daniel Vargas <danielvargas999@gmail.com>
2022-02-17 09:45:43 -0500

    Adjust on script

--- db_scripts/script.sql
+++ db_scripts/script.sql
@@ -7,21 +7,21 @@ SET FOREIGN_KEY_CHECKS=0
 
 /* Drop Tables */
 
-DROP TABLE IF EXISTS `Employee` CASCADE
+DROP TABLE IF EXISTS `employee` CASCADE
 ;
 
 /* Create Tables */
 
-CREATE TABLE `Employee`
+CREATE TABLE `employee`
 (
 	`id` VARCHAR(38) NOT NULL,
-	`documentType` VARCHAR(4) NOT NULL,
-	`documentNumber` VARCHAR(30) NOT NULL,
-	`firstName` VARCHAR(50) NULL,
-	`lastName` VARCHAR(50) NULL,
-	`birthDate` DATE NOT NULL,
-	`jobStartDate` DATE NOT NULL,
-	`jobTitle` VARCHAR(100) NOT NULL,
+	`document_type` VARCHAR(4) NOT NULL,
+	`document_number` VARCHAR(30) NOT NULL,
+	`first_name` VARCHAR(50) NULL,
+	`last_name` VARCHAR(50) NULL,
+	`birth_date` DATE NOT NULL,
+	`job_start_date` DATE NOT NULL,
+	`job_title` VARCHAR(100) NOT NULL,
 	`salary` DOUBLE(10,2) NOT NULL,
 	CONSTRAINT `PK_Employee` PRIMARY KEY (`id` ASC)
 )
@@ -30,3 +30,4 @@ CREATE TABLE `Employee`
 
 SET FOREIGN_KEY_CHECKS=1
 ;
+
